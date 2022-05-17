e9f1582929d5dc5e551145d27b04689308328152
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-05-17 17:48:21 +0200

    Spreading data passed to component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,24 @@
 import style from './card.module.css'
 
-export default function Card({img, rating, reviewCount, location, title, price}) {
+export default function Card(props) {
+  let badgeText
+  if (props.openSpots === 0) {
+    badgeText = 'SOLD OUT'
+  } else if (props.location === 'Online') {
+    badgeText = 'ONLINE'
+  }
   return (
-    <div className='card'>
-      <img src={img} alt='Card' className={style.cardImg} />
+    <div className={style.card}>
+      { badgeText && <div className={style.cardBadge}>{badgeText}</div> }
+      <img src={props.coverImg} alt='Card' className={style.cardImg} />
       <div className={style.cardStats}>
         <img src='/images/star.png' alt='star' className={style.cardStar} />
-        <span>{rating}</span>
-        <span className={style.gray}>({reviewCount}) • </span>
-        <span className={style.gray}>{location}</span>
+        <span>{props.stats.rating}</span>
+        <span className={style.gray}>({props.stats.reviewCount}) • </span>
+        <span className={style.gray}>{props.location}</span>
       </div>
-      <p>{title}</p>
-      <p><span className={style.bold}>From ${price}</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className={style.bold}>From ${props.price}</span> / person</p>
     </div>
   )
 }
